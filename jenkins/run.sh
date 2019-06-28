79ffbfde800fcc38539a0d06d217f335d4a764d2
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-06-28 17:20:23 +0300

    finish

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -2,8 +2,9 @@
 docker build ./docker -t itea-jenkins
 docker run \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v /home/vitaliy/.docker:/root/.docker \
 -v $(which docker):/usr/bin/docker \
--e GIT_PASSWORD=Ns9sBV3Itx \
+-e GIT_PASSWORD=$1 \
 -e ENV=prod \
 -e JENKINS_COLOR_SCHEMA=green \
 -p 8080:8080 \
