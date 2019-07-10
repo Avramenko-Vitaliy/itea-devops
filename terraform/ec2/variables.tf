9ed03eb17a531523be88ffa85301cb0e50e8c3a9
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-07-10 19:26:08 +0300

    lesson-6

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "profile_name" { default = "glomex" }
+variable "ami_id" { default = "ami-035b3c7efe6d061d5" }
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
