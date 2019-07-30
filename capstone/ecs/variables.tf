76362dfc0ab356ffa769e034b6a3d04b2fbcf8c5
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-07-30 21:29:05 +0300

    changes

--- capstone/ecs/variables.tf
+++ capstone/ecs/variables.tf
@@ -1,10 +1,8 @@
 # main creds for AWS connection
 variable "aws_access_key_id" {
-  default = "AKIAR4S3KN4UFDN5DO5F"
 }
 
 variable "aws_secret_access_key" {
-  default = "QfCQRIwi5fOHzUlB2m+hSNrrV/1z2609lLadkpB3"
 }
 
 variable "ecs_cluster" {
