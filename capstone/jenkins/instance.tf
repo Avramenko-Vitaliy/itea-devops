e0dc9c95509ec30b43e6ca23d7398c8c785f9d9b
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-07-31 17:44:32 +0300

    changes

--- capstone/jenkins/instance.tf
+++ capstone/jenkins/instance.tf
@@ -28,7 +28,7 @@ sudo service jenkins start
 
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
+sudo mv terraform /usr/bin/
 EOF
 
   tags = {
