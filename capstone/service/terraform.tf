717365f261cd363bd09b450a80bc49a746624df2
Avramenko Vitaliy <vitaliy.avramenko@ardas.biz>
2019-07-31 15:13:48 +0300

    changes

--- capstone/service/terraform.tf
+++ capstone/service/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "itea-devops-vitaliy-avramenko-1"
-    key = "terraform/terraform.tfstate"
+    key = "terraform/service/terraform.tfstate"
     region = "us-east-1"
   }
 }
